7c6078488125292fa820acccf412277f9a840a15
themildmiles (introvsgit) <164811247+themildmiles@users.noreply.github.com>
2025-08-04 19:09:22 +0800

    Modified build script

--- build.ps1
+++ build.ps1
@@ -1,17 +1,16 @@
 Write-Host "Hello there! This will assume you have installed HaxeFlixel correctly. Proceed with compilation?"
 Write-Host "Press [Enter] to continue or [Esc] to cancel..."
 
-# Wait for actual keypress
 do {
     $key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 } while ($key.VirtualKeyCode -ne 13 -and $key.VirtualKeyCode -ne 27)
 
 if ($key.VirtualKeyCode -eq 27) {
-    Write-Host "ESC pressed. Exiting..."
+    Write-Host "Cancelled."
     exit
 }
 
-Write-Host "Enter pressed. Continuing setup..."
+Write-Host "Continuing compilation..."
 
 haxelib setup ./.libs
 haxelib install flixel
@@ -20,3 +19,5 @@ haxelib install lime
 haxelib install openfl
 
 lime build windows
+
+Write-Host "Done! Please go to export/release/windows."
